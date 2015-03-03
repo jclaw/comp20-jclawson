626206fc413233f2fe74a76e0e26f33230a05d58
jclaw <jclaw3456@gmail.com>
2015-03-02 21:25:30 -0500

    game board and ms pacman rendered

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,15 @@
-// Replace this with your code...
+function init() {
+	console.log("init()");
+	var canvas = document.getElementById('game_canvas');
+    var ctx = canvas.getContext('2d');
+    
+	var img = new Image();
+	img.addEventListener("load", function() {
+		ctx.drawImage(img, 322, 2, 464, 136, 0, 0, 464, 136);
+		ctx.drawImage(img, 83, 23, 13, 14, 15, 29, 13, 14);
+	}, false);
+	img.src = 'pacman10-hp-sprite.png';
+    
+
+              
+}
