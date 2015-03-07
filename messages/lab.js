206d2f175bf773ca096b027d0bc5ec5be556b46d
jclaw <jclaw3456@gmail.com>
2015-03-07 12:06:48 -0500

    1st attempt

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,16 @@
-// Your JavaScript goes here...
+function parse() {
+	xhr = new XMLHttpRequest();
+	xhr.open("get", "data.json", true);
+	xhr.onreadystatechange = myCallbackFunction;
+	xhr.send();
+
+
+
+}
+
+function myCallbackFunction() {
+	if (xhr.readyState == 4 && xhr.status == 200) {
+		data = JSON.parse("data.json");
+		document.getElementById("messages").innerHTML = xhr.responseText;
+	}
+}
