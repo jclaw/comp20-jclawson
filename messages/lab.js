f7feb15bbd833b1743be907f96f1553b54b6f3fb
jclaw <jclaw3456@gmail.com>
2015-03-07 12:44:47 -0500

    initial debugging 3rd attempt

--- messages/lab.js
+++ messages/lab.js
@@ -12,9 +12,8 @@ function parse() {
 function myCallbackFunction() {
 	console.log("In my callback function " + xhr.readyState);
 	if (xhr.readyState == 4 && xhr.status == 200) {
-		data = JSON.parse("data.json");
+		data = JSON.parse(xhr.responseText);
 		console.log(data);
-		console.log(xhr.responseText);
 		document.getElementById("messages").innerHTML = xhr.responseText;
 	}
 }
